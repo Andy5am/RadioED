2172ac4b54e74fee805864d0d90bc996b83833d6
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-01-14 14:06:29 -0600

    Se creo la interfaz

---
+++ src/Radio.java
@@ -0,0 +1,10 @@
+public interface Radio {
+    public void toggle();
+    public boolean getState();
+    public void changeFrecuency();
+    public void changeStation(boolean up);
+    public boolean getFrecuency();
+    public void saveStation(int numButton);
+    public void changeStationButton(int numButton);
+    public double getStation();
+}
